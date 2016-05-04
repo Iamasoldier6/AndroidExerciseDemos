f3e85fd64810c712f61f69248f37e7eb102a8cde
Iamasoldier6 <iamasoldiersix@gmail.com>
2016-05-04 15:35:59 +0800

    ImageLoader Of Volley 的使用，加入BitmapCache

---
+++ ImageLoaderCacheDemo/app/src/main/java/com/iamasoldier6/imageloadercachedemo/BitmapCache.java
@@ -0,0 +1,35 @@
+package com.iamasoldier6.imageloadercachedemo;
+
+import android.graphics.Bitmap;
+import android.util.LruCache;
+
+import com.android.volley.toolbox.ImageLoader;
+
+/**
+ * Created by Iamasoldier6 on 5/4/16.
+ */
+public class BitmapCache implements ImageLoader.ImageCache {
+
+    private LruCache<String, Bitmap> mCache;
+
+    public BitmapCache() {
+        int maxSize = 1 * 1024 * 1024;
+        mCache = new LruCache<String, Bitmap>(maxSize) {
+
+            @Override
+            protected int sizeOf(String key, Bitmap bitmap) {
+                return bitmap.getRowBytes() * bitmap.getHeight();
+            }
+        };
+    }
+
+    @Override
+    public Bitmap getBitmap(String url) {
+        return mCache.get(url);
+    }
+
+    @Override
+    public void putBitmap(String url, Bitmap bitmap) {
+        mCache.put(url, bitmap);
+    }
+}
