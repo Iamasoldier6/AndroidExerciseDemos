74024300e08b5ca1b56166dd82b520a182de24f7
Iamasoldier6 <iamasoldiersix@gmail.com>
2016-06-03 08:59:34 +0800

    《Android 群英传》中 View 的测量 Demo

---
+++ ViewMeasureDemo/app/src/main/java/com/iamasoldier6/viewmeasuredemo/ViewMeasure.java
@@ -0,0 +1,73 @@
+package com.iamasoldier6.viewmeasuredemo;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+
+/**
+ * Created by Iamasoldier6 on 6/2/16.
+ */
+public class ViewMeasure extends View {
+
+    public ViewMeasure(Context context) {
+        super(context);
+    }
+
+    public ViewMeasure(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ViewMeasure(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        setMeasuredDimension(measureWidth(widthMeasureSpec),
+                measureHeight(heightMeasureSpec));
+    }
+
+    private int measureWidth(int measureSpec) {
+        int result = 0;
+        int specMode = MeasureSpec.getMode(measureSpec);
+        int specSize = MeasureSpec.getSize(measureSpec);
+
+        if (specMode == MeasureSpec.EXACTLY) {
+            result = specSize;
+        } else {
+            result = 200;
+            if (specMode == MeasureSpec.AT_MOST) {
+                result = Math.min(result, specSize);
+            }
+        }
+        return result;
+    }
+
+    private int measureHeight(int measureSpec) {
+        int result = 0;
+        int specMode = MeasureSpec.getMode(measureSpec);
+        int specSize = MeasureSpec.getSize(measureSpec);
+
+        if (specMode == MeasureSpec.EXACTLY) {
+            result = specSize;
+        } else {
+            result = 200;
+            if (specMode == MeasureSpec.AT_MOST) {
+                result = Math.min(result, specSize);
+            }
+        }
+        return result;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawColor(Color.GRAY);
+        int width = getWidth();
+        int height = getHeight();
+        Log.d("xys", "width : " + width + " height : " + height);
+    }
+}
