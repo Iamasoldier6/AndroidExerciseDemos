7d47ca13627a36e8696ec7451cf2884fd4f8972c
Iamasoldier6 <iamasoldiersix@gmail.com>
2016-06-05 17:19:20 +0800

    《Android 群英传》中动态的文字闪动效果 Demo

---
+++ ShiTextViewDemo/app/src/main/java/com/iamasoldier6/shitextviewdemo/ShiTextView.java
@@ -0,0 +1,64 @@
+package com.iamasoldier6.shitextviewdemo;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Shader;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Created by Iamasoldier6 on 6/5/16.
+ */
+public class ShiTextView extends TextView {
+
+    private LinearGradient mLinearGradient;
+    private Matrix mGradientMatrix;
+    private Paint mPaint;
+    private int mViewWidth = 0;
+    private int mTranslate = 0;
+
+    public ShiTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        if (mViewWidth == 0) {
+            mViewWidth = getMeasuredWidth();
+            if (mViewWidth > 0) {
+                mPaint = getPaint();
+                mLinearGradient = new LinearGradient(
+                        0,
+                        0,
+                        mViewWidth,
+                        0,
+                        new int[]{
+                                Color.BLUE, 0XFFFFFFFF,
+                                Color.BLUE},
+                        null,
+                        Shader.TileMode.CLAMP);
+                mPaint.setShader(mLinearGradient);
+                mGradientMatrix = new Matrix();
+            }
+        }
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        if (mGradientMatrix != null) {
+            mTranslate += mViewWidth / 5;
+            if (mTranslate > 2 * mViewWidth) {
+                mTranslate = -mViewWidth;
+            }
+            mGradientMatrix.setTranslate(mTranslate, 0);
+            mLinearGradient.setLocalMatrix(mGradientMatrix);
+            postInvalidateDelayed(100);
+        }
+    }
+}
