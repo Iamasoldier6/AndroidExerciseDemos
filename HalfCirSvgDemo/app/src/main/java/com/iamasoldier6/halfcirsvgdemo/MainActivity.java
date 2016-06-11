7006f8f002d2ce37818eb611c34c2b6ae4367b7a
Iamasoldier6 <iamasoldiersix@gmail.com>
2016-06-11 16:59:31 +0800

    《Android 群英传》中绘制半圆的 SVG 图形 Demo

---
+++ HalfCirSvgDemo/app/src/main/java/com/iamasoldier6/halfcirsvgdemo/MainActivity.java
@@ -0,0 +1,18 @@
+package com.iamasoldier6.halfcirsvgdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView = (ImageView) findViewById(R.id.image);
+    }
+    
+}
