5fdeb86a51cebb9ec2561a8fd9422f636890bb58
Iamasoldier6 <iamasoldiersix@gmail.com>
2016-06-15 18:09:28 +0800

    《 Android 群英传》中画笔特效处理的刮刮卡效果 Demo

---
+++ ScratchCardDemo/app/src/main/java/com/iamasoldier6/scratchcarddemo/XfermodeView.java
@@ -0,0 +1,80 @@
+package com.iamasoldier6.scratchcarddemo;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * Created by Iamasoldier6 on 6/15/16.
+ */
+public class XfermodeView extends View {
+
+    private Bitmap mBgBitmap, mFgBitmap;
+    private Paint mPaint;
+    private Canvas mCanvas;
+    private Path mPath;
+
+    public XfermodeView(Context context) {
+        super(context);
+        initView();
+    }
+
+    public XfermodeView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initView();
+    }
+
+    public XfermodeView(Context context, AttributeSet attrs,
+                        int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initView();
+    }
+
+    private void initView() {
+        mPaint = new Paint();
+        mPaint.setAlpha(0);
+        mPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_IN));
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeJoin(Paint.Join.ROUND);
+        mPaint.setStrokeWidth(50);
+        mPaint.setStrokeCap(Paint.Cap.ROUND);
+        mPath = new Path();
+        mBgBitmap = BitmapFactory.decodeResource(getResources(),
+                R.drawable.beauty);
+        mFgBitmap = Bitmap.createBitmap(mBgBitmap.getWidth(), mBgBitmap.getHeight(),
+                Bitmap.Config.ARGB_8888);
+        mCanvas = new Canvas(mFgBitmap);
+        mCanvas.drawColor(Color.GRAY);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                mPath.reset();
+                mPath.moveTo(event.getX(), event.getY());
+                break;
+            case MotionEvent.ACTION_MOVE:
+                mPath.lineTo(event.getX(), event.getY());
+                break;
+        }
+        mCanvas.drawPath(mPath, mPaint);
+        invalidate();
+        return true;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawBitmap(mBgBitmap, 0, 0, null);
+        canvas.drawBitmap(mFgBitmap, 0, 0, null);
+    }
+}
