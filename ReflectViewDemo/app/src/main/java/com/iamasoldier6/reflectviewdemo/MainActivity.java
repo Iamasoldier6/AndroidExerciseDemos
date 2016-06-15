177c3fe3d8e3ed515571182e6b75d56e9b269a83
Iamasoldier6 <iamasoldiersix@gmail.com>
2016-06-15 20:56:30 +0800

    《Android 群英传》中具有倒影效果的 Demo

---
+++ ReflectViewDemo/app/src/main/java/com/iamasoldier6/reflectviewdemo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.iamasoldier6.reflectviewdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+        setContentView(new ReflectView(this));
+    }
+}
