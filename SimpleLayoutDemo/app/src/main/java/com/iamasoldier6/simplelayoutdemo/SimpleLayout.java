0df8d92a51a928077828b7b1d04955c45a51d390
Iamasoldier6 <iamasoldiersix@gmail.com>
2016-08-21 09:17:48 +0800

    理解 onLayout() 的过程

---
+++ SimpleLayoutDemo/app/src/main/java/com/iamasoldier6/simplelayoutdemo/SimpleLayout.java
@@ -0,0 +1,42 @@
+package com.iamasoldier6.simplelayoutdemo;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Iamasoldier6 on 8/21/16.
+ */
+public class SimpleLayout extends ViewGroup {
+
+
+    public SimpleLayout(Context context) {
+        super(context);
+    }
+
+    public SimpleLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SimpleLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        if (getChildCount() > 0) {
+            View childView = getChildAt(0);
+            measureChild(childView, widthMeasureSpec, heightMeasureSpec);
+        }
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        if (getChildCount() > 0) {
+            View childView = getChildAt(0);
+            childView.layout(0, 0, childView.getMeasuredWidth(), childView.getMeasuredHeight());
+        }
+    }
+}
