9d138c9f7b00588a538100568c6fc9306b2aa96c
Iamasoldier6 <iamasoldiersix@gmail.com>
2017-03-13 23:45:35 +0800

    二阶贝塞尔曲线

--- QuadBezierLawDemo/app/src/main/java/com/iamasoldier6/quadbezierlawdemo/QuadBezierView.java
+++ QuadBezierLawDemo/app/src/main/java/com/iamasoldier6/quadbezierlawdemo/QuadBezierView.java
@@ -23,36 +23,36 @@ public class QuadBezierView extends View {
 
     private PointF start, end, control;
 
-    public QuadBezierView( Context context ) {
-        super( context );
+    public QuadBezierView(Context context) {
+        super(context);
         initView();
     }
 
-    public QuadBezierView( Context context, @Nullable AttributeSet attrs ) {
-        super( context, attrs );
+    public QuadBezierView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
         initView();
     }
 
-    public QuadBezierView(Context context, @Nullable AttributeSet attrs, int defStyleAttr ) {
-        super( context, attrs, defStyleAttr );
+    public QuadBezierView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
         initView();
     }
 
     private void initView() {
         mPaint = new Paint();
-        mPaint.setColor( Color.BLACK );
-        mPaint.setStrokeWidth( 8 );
-        mPaint.setStyle( Paint.Style.STROKE );
-        mPaint.setTextSize( 60 );
+        mPaint.setColor(Color.BLACK);
+        mPaint.setStrokeWidth(8);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setTextSize(60);
 
-        start = new PointF( 0, 0 );
-        end = new PointF( 0, 0 );
-        control = new PointF( 0, 0 );
+        start = new PointF(0, 0);
+        end = new PointF(0, 0);
+        control = new PointF(0, 0);
     }
 
     @Override
-    protected void onSizeChanged( int w, int h, int oldw, int oldh ) {
-        super.onSizeChanged( w, h, oldw, oldh );
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
         centerX = w / 2;
         centerY = h / 2;
 
@@ -68,7 +68,7 @@ public class QuadBezierView extends View {
     }
 
     @Override
-    public boolean onTouchEvent( MotionEvent event ) {
+    public boolean onTouchEvent(MotionEvent event) {
         // 根据触摸位置更新控制点，并提示重绘
         control.x = event.getX();
         control.y = event.getY();
@@ -77,30 +77,30 @@ public class QuadBezierView extends View {
     }
 
     @Override
-    protected void onDraw( Canvas canvas ) {
-        super.onDraw( canvas );
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
 
         // 绘制数据点和控制点
-        mPaint.setColor( Color.GRAY );
-        mPaint.setStrokeWidth( 20 );
-        canvas.drawPoint( start.x, start.y, mPaint );
-        canvas.drawPoint( end.x, end.y, mPaint );
-        canvas.drawPoint( control.x, control.y, mPaint );
+        mPaint.setColor(Color.GRAY);
+        mPaint.setStrokeWidth(20);
+        canvas.drawPoint(start.x, start.y, mPaint);
+        canvas.drawPoint(end.x, end.y, mPaint);
+        canvas.drawPoint(control.x, control.y, mPaint);
 
         // 绘制辅助线
-        mPaint.setStrokeWidth( 4 );
-        canvas.drawLine( start.x, start.y, control.x, control.y, mPaint );
-        canvas.drawLine( end.x, end.y, control.x, control.y, mPaint );
+        mPaint.setStrokeWidth(4);
+        canvas.drawLine(start.x, start.y, control.x, control.y, mPaint);
+        canvas.drawLine(end.x, end.y, control.x, control.y, mPaint);
 
         // 绘制贝塞尔曲线
-        mPaint.setColor( Color.RED );
-        mPaint.setStrokeWidth( 8 );
+        mPaint.setColor(Color.RED);
+        mPaint.setStrokeWidth(8);
 
         Path path = new Path();
 
-        path.moveTo( start.x, start.y );
-        path.quadTo( control.x, control.y, end.x, end.y );
-        canvas.drawPath( path, mPaint );
+        path.moveTo(start.x, start.y);
+        path.quadTo(control.x, control.y, end.x, end.y);
+        canvas.drawPath(path, mPaint);
     }
 
 }
