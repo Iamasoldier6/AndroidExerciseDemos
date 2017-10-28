114cfad770067a057d04c5c478b7c206470e7ae9
Iamasoldier6 <iamasoldiersix@gmail.com>
2017-10-28 21:12:04 +0800

    Glide 的简单使用 Demo

--- GlideDemo/app/src/main/java/com/iamasoldier6/glidedemo/MainActivity.java
+++ GlideDemo/app/src/main/java/com/iamasoldier6/glidedemo/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,8 +17,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mImageView = (ImageView) findViewById(R.id.iv_picture);
 
-        String url = "http://img.my.csdn.net/uploads/201309/01/1378037194_2965.jpg";
-        Glide.with(this).load(url).into(mImageView);
+//        String url = "http://img.my.csdn.net/uploads/201309/01/1378037194_2965.jpg";
+        String url = "http://i.dimg.cc/61/a1/f0/6b/e9/be/3f/f1/f4/f8/4c/65/3c/63/b5/a2.gif";
+        Glide.with(this)
+                .asBitmap()
+                .load(url)
+                .apply(new RequestOptions()
+                        .override(200, 200))
+                .into(mImageView);
+
+//        Glide.with(this)
+//                .load(url)
+//                .apply(new RequestOptions()
+//                        .placeholder(R.drawable.ic_load))
+//                .into(mImageView);
+
+//        Glide.with(this)
+//                .load(url)
+//                .apply(new RequestOptions()
+//                        .placeholder(R.drawable.ic_load))
+//                .apply(new RequestOptions()
+//                        .error(R.drawable.ic_load_fail))
+//                .into(mImageView);
     }
 
 }
